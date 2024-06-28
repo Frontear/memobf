3556e44a986ff037b331151e81f4cb679f793c3e
Ali Rizvi <perm-iterate-0b@icloud.com>
2024-06-28 14:43:43 -0400

    feat: shell.nix -> flake.nix

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+  };
+
+  outputs = inputs@{ flake-parts, ... }: flake-parts.lib.mkFlake { inherit inputs; } {
+    systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
+    perSystem = { config, self', inputs', pkgs, system, ... }: {
+      devShells.default = pkgs.mkShell {
+        packages = with pkgs; [
+          gnumake
+          man-pages
+        ];
+      };
+    };
+  };
+}
--- shell.nix
+++
@@ -1,7 +0,0 @@
-{ pkgs ? import <nixpkgs> {} }: pkgs.mkShell {
-    packages = with pkgs; [
-        gnumake
-        libgcc
-        man-pages
-    ];
-}
